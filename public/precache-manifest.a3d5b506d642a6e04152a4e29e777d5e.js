5a151962d31718eb30f1cc4779889db423170786
Simeon <simogeorgiew@gmail.com>
2020-04-14 16:15:18 +0300

    Initial Commit

---
+++ public/precache-manifest.a3d5b506d642a6e04152a4e29e777d5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05ca9565ac41170170fc3bf48de76217",
+    "url": "/index.html"
+  },
+  {
+    "revision": "126ad4d304349537d679",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d66e996799891fa13a26",
+    "url": "/static/js/2.b7f071b3.chunk.js"
+  },
+  {
+    "revision": "f0d43bb23094b147d2c500466b241ef2",
+    "url": "/static/js/2.b7f071b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "126ad4d304349537d679",
+    "url": "/static/js/main.0a5783af.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  },
+  {
+    "revision": "da69bceeda09b18559a93cadcd7e5a49",
+    "url": "/static/media/spacexlogo.da69bcee.PNG"
+  }
+]);
